478ee7d9d6d4b8674af784e9331c8b98348e9e97
“Cody <cbandrowski@gmail.com//>
2025-03-04 20:26:52 -0500

    working on main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLOutput;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       // PQList pqList = new PQList();
+        PQHeap pqHeap = new PQHeap();
         File file = new File("/Users/cbandrowski/School/CSC311/CSC329_Assignment2/src/input.txt");
 
         if (!file.exists()) {
@@ -20,10 +21,17 @@ public class Main {
             while((nameline = br.readLine()) != null){
                 String scoreLine = br.readLine();
                 int score = Integer.parseInt(scoreLine.trim());
-            //    pqList.add(new Player(nameline, score));
+               pqHeap.add(new Player(nameline, score));
             }
         } catch (IOException | NumberFormatException e) {
             System.out.println("Error reading file");
+        }
+
+
+
+        //Display and remove players in order of highest score
+        while (!pqHeap.isEmpty()) {
+            System.out.println("Highest Score player: "+ pqHeap.getHighestScoreplayer());
         }
     }
 }
--- src/PQHeap.java
+++ src/PQHeap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * PQHeap class - Implements a max heap using an array to
  * store player instances based on their score
@@ -6,6 +8,38 @@
  */
 public class PQHeap implements PriorityQueue{
     private Player[] heap;
+    private int size;
+
+    /**
+     * Default Constructor
+     * Starts heap with size of 10
+     */
+    public PQHeap(){
+        heap = new Player[10];
+        size = 0;
+    }
+
+    /**
+     * Copy Constructor
+     * Creates deep copy of another PQHeap
+     * @param other PQHeap to copy
+     */
+    public PQHeap(PQHeap other ){
+        this.size = other.size;
+        this.heap = new Player[other.size];
+        for(int i = 0; i<size; i++){
+            this.heap[i] = new Player(other.heap[i]);
+        }
+    }
+
+    /**
+     * CreateClone
+     * Create a deep copy of the current PQHeap
+     * @return a new PQHeap with same data
+     */
+    public PQHeap createClone(){
+        return new PQHeap(this);
+    }
 
     /**
      * add method
@@ -15,7 +49,16 @@ public class PQHeap implements PriorityQueue{
      */
     @Override
     public void add(Player a) {
-
+        if(size == heap.length){
+            Player[] temp = new Player[heap.length*2];
+            for(int i = 0; i<size; i++){
+                temp[i] = new Player(heap[i]);
+            }
+            heap = temp;
+        }
+        heap[size] = a;
+        heapifyUp(size);
+        size++;
     }
 
     /**
@@ -27,7 +70,14 @@ public class PQHeap implements PriorityQueue{
      */
     @Override
     public Player getHighestScoreplayer() {
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        Player highest = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        heapifyDown(0);
+        return highest;
     }
 
     /**
@@ -36,6 +86,8 @@ public class PQHeap implements PriorityQueue{
      */
     @Override
     public void clear() {
+        size = 0;
+            heap = new Player[10];
 
     }
 
@@ -43,21 +95,65 @@ public class PQHeap implements PriorityQueue{
      * getSize method
      * Returns the number of players being stored in the priority queue
      *
-     * @return
+     * @return int size
      */
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     /**
      * isEmpty method
      * Returns true if the priority queue is empty.
      *
-     * @return
+     * @return true if empty False If contains data
      */
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    /**
+     * Swap two elements in the heap
+     * @param i first index
+     * @param j second index
+     */
+    private void swap(int i, int j) {
+        Player temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    /**
+     * Heapify up operation to maintain heap property after insertion
+     * @param index of newly added element
+     */
+    private void heapifyUp(int index){
+        int parentIndex = (index-1)/2;
+        while(index > 0 && heap[parentIndex].getScore() > heap[index].getScore()){
+            swap(index, parentIndex);
+            index = parentIndex;
+            parentIndex = (index-1)/2;
+        }
+    }
+
+    /**
+     * Heapidy down operation to maintain heap property after removal
+     * @param index to heapify down
+     */
+    private void heapifyDown(int index){
+        int largest = index;
+        int leftChild = 2*index + 1;
+        int rightChild = 2*index + 2;
+        if(leftChild < size && heap[leftChild].getScore() > heap[largest].getScore()){
+            largest = leftChild;
+        }
+        if(rightChild < size && heap[rightChild].getScore() > heap[largest].getScore()){
+            largest = rightChild;
+        }
+        if(largest != index){
+            swap(index, largest);
+            heapifyDown(largest);
+        }
     }
 }
